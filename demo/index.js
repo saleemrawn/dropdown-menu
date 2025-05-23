e082f7727dee375e60828977f54a00069aebef3d
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-05-23 14:54:42 +0100

    Add HTML, JS for demo of dropdown menu

---
+++ demo/index.js
@@ -0,0 +1,10 @@
+function initializeDropdown() {
+  document.addEventListener("click", (event) => {
+    if (event.target.matches(".dropdown")) {
+      const dropdownMenu = event.target.nextElementSibling;
+      dropdownMenu.classList.toggle("visible");
+    }
+  });
+}
+
+initializeDropdown();
